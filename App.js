6ada6d253d18f7a993afccb222b4449526e20e15
Colton <kaiserc2@seattleu.edu>
2020-10-03 15:51:23 -0700

    Remove APIs and create first stylesheets.

--- App.js
+++ App.js
@@ -1,35 +1,30 @@
 import React from "react";
-import { 
-  Text,
-  View, 
-  ActivityIndicator, 
-  ProgressViewIOS, 
-  Button,
-  Alert,
-  Dimensions,
-  Platform,
-  ProgressBarAndroid
-} from "react-native";
-
-const {height, width} = Dimensions.get('window')
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  const onButtonPress = () => {
-    Alert.alert(`${new Date().toLocaleTimeString()} button press`)
-  }
   return (
-    <View style={{ padding: 50 }}>
-      {Platform.OS === "ios" && <ProgressViewIOS progress={0.5}/>}
-      {Platform.OS === "android" && <ProgressBarAndroid 
-      styleAttr="Horizontal"
-      indeterminate={false}
-      color="blue"
-      progress={0.5}/>}
-      <ActivityIndicator size="large" color="#61DBFB" />
-      <Button title="click me" onPress={onButtonPress}/>
-      <Text>OS: {Platform.OS}</Text>
-  <Text>Height: {height}</Text>
-  <Text>Width: {width}</Text>
+    <View style={styles.page}>
+      <Text style={styles.text}>red</Text>
+      <Text style={[styles.text, styles.selectedText]}>green</Text>
+      <Text style={styles.text}>blue</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  page: {
+    marginTop: 40,
+    backgroundColor: "#DDD",
+  },
+  text: {
+    fontSize: 22,
+    color: "red",
+    backgroundColor: "yellow",
+    margin: 10,
+    padding: 5
+  },
+  selectedText: {
+    backgroundColor: "red",
+    color: "yellow"
+  }
+})
