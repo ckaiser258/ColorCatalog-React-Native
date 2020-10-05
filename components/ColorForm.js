0fd3c17788cf3cb4104bc77b2b8bb0aa2723cd83
Colton <kaiserc2@seattleu.edu>
2020-10-05 15:48:56 -0700

    Add input functionality to Color Form

--- components/ColorForm.js
+++ components/ColorForm.js
@@ -1,11 +1,28 @@
-import React from "react";
+import React, {useState, useRef} from "react";
 import { StyleSheet, View, TextInput, Button } from "react-native";
 
 export default function ColorForm() {
+    const [inputValue, setValue] = useState("")
+    const input = useRef() 
+    //Set up a variable 'input' to be used when wanting to reference a component
   return (
     <View style={styles.container}>
-      <TextInput style={styles.txtInput} autoCapitalize="none"/>
-      <Button title="add" />
+      <TextInput style={styles.txtInput} 
+      ref={input} //Allow this component to be referenced so we can access its properties
+      value={inputValue}
+      onChangeText={setValue}
+      autoCapitalize="none"
+      placeholder="enter a color..."
+      />
+      <Button title="add" 
+      onPress={() => {
+          //Grab the TextInput property method "blur()"
+          input.current.blur()
+          //blur() deselects the text input and causes keyboard to disappear
+          console.log(`add value ${inputValue}`)
+          setValue("")
+
+      }} />
     </View>
   );
 }
