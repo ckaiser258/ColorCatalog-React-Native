b903c5a1dcbc71cdb8c64897f3d0ae4873f02951
Colton <kaiserc2@seattleu.edu>
2020-10-05 14:12:11 -0700

    Remove unnecessary flexbox attributes

--- App.js
+++ App.js
@@ -4,9 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.page}>
-      <Text style={styles.text}>red</Text>
-      <Text style={[styles.text, styles.selectedText]}>green</Text>
-      <Text style={styles.text}>blue</Text>
+      <Text style={styles.text}>[TODO: INSERT CAT]</Text>
     </View>
   );
 }
@@ -14,23 +12,11 @@ export default function App() {
 const styles = StyleSheet.create({
   page: {
     flex: 1,
-    flexDirection: "column",
-    alignItems: "flex-start",
-    justifyContent: "space-around",
-    marginTop: 40,
-    backgroundColor: "#DDD",
+    justifyContent: "center",
+    alignItems: "center"
   },
   text: {
     textAlign: "center",
     fontSize: 22,
-    color: "red",
-    backgroundColor: "yellow",
-    margin: 10,
-    padding: 5
-  },
-  selectedText: {
-    alignSelf: "flex-end",
-    backgroundColor: "red",
-    color: "yellow"
   }
 })
