9eb157482d01f3c83ba6380dda0c48738d7ad354
Colton <kaiserc2@seattleu.edu>
2020-10-05 15:52:49 -0700

    Add and pass onNewColor function to alert typed color

--- App.js
+++ App.js
@@ -1,24 +1,29 @@
 import React, { useState } from "react";
-import { StyleSheet, FlatList } from "react-native";
+import { StyleSheet, FlatList, Alert } from "react-native";
 import ColorButton from "./components/ColorButton";
 import defaultColors from "./data/defaultColors.json";
-import ColorForm from "./components/ColorForm"
+import ColorForm from "./components/ColorForm";
 
 export default function App() {
   const [backgroundColor, setBackgroundColor] = useState("blue");
   return (
     <>
-    <ColorForm/>
-    <FlatList
-      style={[styles.container, { backgroundColor }]}
-      data={defaultColors}
-      renderItem={({ item }) => {
-        return <ColorButton 
-        key={item.id} 
-        backgroundColor={item.color}
-        onPress={setBackgroundColor} />;
-      }}
-    />
+      <ColorForm
+        onNewColor={(newColor) => Alert.alert(`TODO: add color ${newColor}`)}
+      />
+      <FlatList
+        style={[styles.container, { backgroundColor }]}
+        data={defaultColors}
+        renderItem={({ item }) => {
+          return (
+            <ColorButton
+              key={item.id}
+              backgroundColor={item.color}
+              onPress={setBackgroundColor}
+            />
+          );
+        }}
+      />
     </>
   );
 }
--- components/ColorForm.js
+++ components/ColorForm.js
@@ -1,28 +1,30 @@
-import React, {useState, useRef} from "react";
+import React, { useState, useRef } from "react";
 import { StyleSheet, View, TextInput, Button } from "react-native";
 
-export default function ColorForm() {
-    const [inputValue, setValue] = useState("")
-    const input = useRef() 
-    //Set up a variable 'input' to be used when wanting to reference a component
+export default function ColorForm({onNewColor=f => f}) {
+  const [inputValue, setValue] = useState("");
+  const input = useRef();
+  //Set up a variable 'input' to be used when wanting to reference a component
   return (
     <View style={styles.container}>
-      <TextInput style={styles.txtInput} 
-      ref={input} //Allow this component to be referenced so we can access its properties
-      value={inputValue}
-      onChangeText={setValue}
-      autoCapitalize="none"
-      placeholder="enter a color..."
+      <TextInput
+        style={styles.txtInput}
+        ref={input} //Allow this component to be referenced so we can access its properties
+        value={inputValue}
+        onChangeText={setValue}
+        autoCapitalize="none"
+        placeholder="enter a color..."
       />
-      <Button title="add" 
-      onPress={() => {
+      <Button
+        title="add"
+        onPress={() => {
           //Grab the TextInput property method "blur()"
-          input.current.blur()
+          input.current.blur();
           //blur() deselects the text input and causes keyboard to disappear
-          console.log(`add value ${inputValue}`)
-          setValue("")
-
-      }} />
+          onNewColor(inputValue)
+          setValue("");
+        }}
+      />
     </View>
   );
 }
@@ -31,7 +33,7 @@ const styles = StyleSheet.create({
   container: {
     marginTop: 40,
     flexDirection: "row",
-    alignItems: "center"
+    alignItems: "center",
   },
   txtInput: {
     flex: 1,
@@ -39,6 +41,6 @@ const styles = StyleSheet.create({
     fontSize: 20,
     margin: 5,
     borderRadius: 5,
-    padding: 5
+    padding: 5,
   },
 });
