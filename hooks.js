74d4fefef8115d9aa03cae69118d7678d1b61468
Colton <kaiserc2@seattleu.edu>
2020-10-06 13:11:23 -0700

    Add async storage functionality for colors

--- hooks.js
+++ hooks.js
@@ -1,8 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { generate } from "shortid";
+import {AsyncStorage} from 'react-native'
 
 export const useColors = () => {
     const [colors, setColors] = useState([]);
+
+    const loadColors = async () => {
+        const colorData = await AsyncStorage.getItem("@ColorListStore:Colors") //Key for the data we want to load.
+        if (colorData) {
+            const colors = JSON.parse(colorData)
+            //Convert string into an array of colors.
+            setColors(colors)
+        }
+    }
+
+    //Load colors upon render.
+    useEffect(() => {
+        if (colors.length) return
+        loadColors()
+    }, [])
+    
+    //Any time there's a change to the colors array, save that change to AsyncStorage.
+    useEffect(() => {
+        AsyncStorage.setItem("@ColorListStore:Colors", JSON.stringify(colors))
+    }, [colors])
+
     const addColor = (color) => {
       const newColor = { id: generate(), color };
       setColors([newColor, ...colors]);
