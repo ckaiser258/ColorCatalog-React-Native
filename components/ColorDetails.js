85b6742ad4e88e911ce1c41354cb579819cdaa7d
Colton <kaiserc2@seattleu.edu>
2020-10-06 13:28:36 -0700

    Fix ColorDetails dependencies

--- components/ColorDetails.js
+++ components/ColorDetails.js
@@ -1,14 +1,11 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 
 export default function ColorDetails() {
-  const [backgroundColor, setBackgroundColor] = useState("blue");
-  const { colors, addColor } = useColors();
-
   return (
-      <View style={styles.container}>
-          <Text>Color Details</Text>
-      </View>
+    <View style={styles.container}>
+      <Text>Color Details</Text>
+    </View>
   );
 }
 
@@ -17,6 +14,6 @@ const styles = StyleSheet.create({
     flex: 1,
     display: "flex",
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
   },
 });
